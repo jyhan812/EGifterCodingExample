2971830aca5a0fcb77dfd64b6082b2f7645209b8
jyhan812 <32969446+jyhan812@users.noreply.github.com>
2017-10-27 00:06:45 -0400

    Change Stack type - from string to int

--- MatchClass/Match.cs
+++ MatchClass/Match.cs
@@ -12,12 +12,29 @@ namespace MatchClass
         {
             bool isMatched = true;
 
-            Stack<string> stack = new Stack<string>();
+            Stack<int> stack = new Stack<int>();
 
             //logic
             //1.when you see ( push it and pop stack when you see ) 
             //2.if stack has no ( but if ) entered, it fails
             //3.finally when the string is parsed completely the stack should be empty
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '(':
+
+                        break;
+                    case ')':
+
+                        break;
+                    default:
+
+                        break;
+                }
+
+            }
 
             return isMatched;
         }
